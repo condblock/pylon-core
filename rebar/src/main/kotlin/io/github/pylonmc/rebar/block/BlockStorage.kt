3621495442234fa4faf0014bf43390f4d33e3e7c
Idra <percy.baker.0@gmail.com>
2026-03-09 13:10:11 +0000

    lol that was a problem in all the others too

--- rebar/src/main/kotlin/io/github/pylonmc/rebar/block/BlockStorage.kt
+++ rebar/src/main/kotlin/io/github/pylonmc/rebar/block/BlockStorage.kt
@@ -208,24 +208,24 @@ object BlockStorage : Listener {
         }
 
     /**
-     * Returns whether the block at [blockPosition] is a Rebar block, or null if the
-     * chunk at [blockPosition] is not loaded
+     * Returns whether the block at [blockPosition] is a Rebar block.
+     * Returns false if the chunk at [blockPosition] is not loaded.
      */
     @JvmStatic
     fun isRebarBlock(blockPosition: BlockPosition): Boolean =
         (blockPosition.chunk.isLoaded) && get(blockPosition) != null
 
     /**
-     * Returns whether the block at [block] is a Rebar block, or null if the
-     * chunk at [block] is not loaded
+     * Returns whether the block at [block] is a Rebar block.
+     * Returns false if the chunk at [blockPosition] is not loaded.
      */
     @JvmStatic
     fun isRebarBlock(block: Block): Boolean =
         (block.position.chunk.isLoaded) && get(block) != null
 
     /**
-     * Returns whether the block at [location] is a Rebar block, or null if the
-     * chunk at [location] is not loaded
+     * Returns whether the block at [location] is a Rebar block
+     * Returns false if the chunk at [blockPosition] is not loaded.
      */
     @JvmStatic
     fun isRebarBlock(location: Location): Boolean =
