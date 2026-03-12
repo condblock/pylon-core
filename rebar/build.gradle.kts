7741952488a6ad897595fb3a0f688dc2d1e90a12
ybw0014 <i@guizhanss.com>
2026-03-12 14:22:08 -0400

    feat: specify command used by jitpack

--- rebar/build.gradle.kts
+++ rebar/build.gradle.kts
@@ -176,7 +176,7 @@ publishing {
         create<MavenPublication>("maven") {
             artifactId = "rebar"
 
-            artifact(tasks.shadowJar)
+            artifact(tasks.jar)
             artifact(tasks.kotlinSourcesJar)
             artifact(javadocJar)
 
